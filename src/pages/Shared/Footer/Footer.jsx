eec3954303ac7c6ec3702ea3af29704613ff6feb
BakiAbdullah <shatil605@gmail.com>
2023-05-02 00:27:55 +0600

    Added header & footer page

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -3,7 +3,41 @@ import React from 'react';
 const Footer = () => {
   return (
     <div>
-      
+      <footer className="footer mt-16 p-10 bg-gray-900 text-gray-100">
+        <div>
+          <h2 className="font-bold text-3xl">TastyBites</h2>
+          <p>
+            Providing reliable service since 2013
+          </p>
+          <img src="/src/assets/Icons/footer.png" alt="" />
+        </div>
+        <div>
+          <span className="footer-title">Company</span>
+          <a className="link link-hover">Branding</a>
+          <a className="link link-hover">Design</a>
+          <a className="link link-hover">Marketing</a>
+          <a className="link link-hover">Advertisement</a>
+        </div>
+        <div>
+          <span className="footer-title">Product</span>
+          <a className="link link-hover">About us</a>
+          <a className="link link-hover">Contact</a>
+          <a className="link link-hover">Jobs</a>
+          <a className="link link-hover">Press kit</a>
+        </div>
+        <div>
+          <span className="footer-title">Support</span>
+          <a className="link link-hover">Help Desk</a>
+          <a className="link link-hover">Sales</a>
+          <a className="link link-hover">Become a Partner</a>
+          <a className="link link-hover">Developers</a>
+        </div>
+        <div>
+          <span className="footer-title">Contact</span>
+          <a className="link link-hover">524 Broadway , NYC</a>
+          <a className="link link-hover">+1 777 - 978 - 5570</a>
+        </div>
+      </footer>
     </div>
   );
 };
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layouts/Main";
+import Home from "../pages/Home/Home";
+import Blogs from "../pages/Blogs/Blogs";
 
-const routes = () => {
-  return (
-    <div>
-      
-    </div>
-  );
-};
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: '/blog',
+        element: <Blogs></Blogs>
+      }
+    ],
+  },
 
-export default routes;
+  // {
+  //   path: "/",
+  //   element: <LoginLayout></LoginLayout>,
+  //   children: [
+  //     {
+  //       path: "/login",
+  //       element: <Login></Login>,
+  //     },
+  //     {
+  //       path: "/register",
+  //       element: <Registration></Registration>,
+  //     },
+  //   ],
+  // },
+]);
+
+export default router;
