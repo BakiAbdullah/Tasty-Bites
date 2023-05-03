d4ab2258c44823333d850f99a2848810780dd648
BakiAbdullah <shatil605@gmail.com>
2023-05-03 10:18:34 +0600

    Recipe Details card data shown

--- src/pages/Home/ChefDetails.jsx
+++ src/pages/Home/ChefDetails.jsx
@@ -1,7 +1,8 @@
 import React, { useContext } from "react";
 import { Link, useLoaderData, useParams } from "react-router-dom";
 import { AuthContext } from "../../providers/AuthProvider";
-import { FaLongArrowAltRight, FaThumbsUp } from "react-icons/fa";
+
+import ChefDetailsCard from "./ChefDetailsCard";
 
 const ChefDetails = () => {
   const allChefdata = useContext(AuthContext);
@@ -20,59 +21,12 @@ const ChefDetails = () => {
       </h2>
 
       <div className="grid px-10 gap-8 container mx-auto grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-        {chefDatas.map((chefData) => {
-          return (
-            <div className="card w-full bg-base-100 shadow-xl">
-              <figure>
-                <img
-                  className="w-full h-[650px] object-cover"
-                  src={chefData.picture}
-                />
-              </figure>
-              <div className="card-body">
-                <h2 className="card-title">
-                  {chefData.name}
-                  <div className="badge badge-secondary">
-                    {" "}
-                    <div className="flex items-center p-2">
-                      <FaThumbsUp className="mr-2"></FaThumbsUp>{" "}
-                      {chefData.likes}
-                    </div>{" "}
-                  </div>
-                </h2>
-                <p className="my-3">{chefData.bio.slice(0, 125)}...</p>
-                <div className="">
-                  <div className="space-y-3">
-                    <button
-                      type="button"
-                      className="border-2 border-red-800  text-black px-6 py-1  leading-none flex items-center"
-                    >
-                      Years of Experience{" "}
-                      <span className="p-1 rounded text-red-600 font-semibold ml-2">
-                        {chefData.yearOfExp}
-                      </span>
-                    </button>
-                    <button
-                      type="button"
-                      className="border-2 border-red-800  text-black px-6 py-1  leading-none flex items-center"
-                    >
-                      Number Of Recipes{" "}
-                      <span className="p-1 rounded text-red-600 font-semibold ml-2">
-                        {chefData.numOfRecipes}
-                      </span>
-                    </button>
-                  </div>
-                  <Link to="/recipedetails">
-                    <button className="bg-amber-500 text-black px-6 py-2 mt-8 rounded-md">
-                      View Recipes{" "}
-                      <FaLongArrowAltRight className="inline-block text-red-800 duration-150 hover:transition-all hover:translate-x-1"></FaLongArrowAltRight>{" "}
-                    </button>
-                  </Link>
-                </div>
-              </div>
-            </div>
-          );
-        })}
+        {chefDatas.map((chefData) => (
+          <ChefDetailsCard
+            key={chefData.chefId}
+            value={chefData}
+          ></ChefDetailsCard>
+        ))}
       </div>
     </div>
   );
--- src/pages/RecipesDetails/RecipesDetails.jsx
+++ src/pages/RecipesDetails/RecipesDetails.jsx
@@ -1,20 +1,29 @@
 import React, { useContext, useState } from "react";
-import { useLoaderData, useParams } from "react-router-dom";
+import { useLoaderData, useLocation, useParams } from "react-router-dom";
 import { AuthContext } from "../../providers/AuthProvider";
 import RecipeDetailsCard from "./RecipeDetailsCard";
+import ChefBannerCard from "./ChefBannerCard";
 
 const RecipesDetails = () => {
-  
+  const singleChefdata = useLocation().state;
+  const chefName = singleChefdata.name;
+
+  console.log(singleChefdata);
 
   const { recipesData } = useContext(AuthContext);
-  console.log(recipesData);
+  // console.log(recipesData);
   // const { id } = useParams();
   // const singleChefData = useLoaderData();
   // console.log(singleChefData);
 
   return (
     <div className="container mx-auto my-12">
-      <h2>Chef dataaaaaaa!!</h2>
+      <div>
+        <ChefBannerCard value={singleChefdata}></ChefBannerCard>
+      </div>
+      <h2 className="text-center text-3xl font-bold my-4">
+        3 best dishes of <span className="text-amber-500">{chefName}</span>
+      </h2>
       <div className="grid lg:grid-cols-3 grid-cols-1 gap-8">
         {recipesData.slice(0, 3).map((recipe) => (
           <RecipeDetailsCard
